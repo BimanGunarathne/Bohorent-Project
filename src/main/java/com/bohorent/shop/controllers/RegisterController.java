211b8b3c47e3be815650bbc0bbad1d2b17de236b
Biman Gunarathne <bimangunarathne@outlook.com>
2023-06-02 11:29:54 +0530

    Updated application.properties, LoginController.java, pom.xml, RegisterController.java, VerificationMail.java, new added EmailVerificationController.java

--- src/main/java/com/bohorent/shop/controllers/RegisterController.java
+++ src/main/java/com/bohorent/shop/controllers/RegisterController.java
@@ -15,6 +15,7 @@ import org.hibernate.query.Query;
 
 import javax.persistence.NoResultException;
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Routes
 public class RegisterController {
@@ -40,13 +41,16 @@ public class RegisterController {
         user.setEmail(request.getParameter("email"));
         user.setPassword(Encryption.encrypt(request.getParameter("password")));
 
+        String verificationCode = UUID.randomUUID().toString();
+        user.setVerification_code(verificationCode);
+
         Transaction transaction = session.beginTransaction();
 
         session.save(user);
         transaction.commit();
         session.close();
 
-        VerificationMail mail = new VerificationMail(user.getEmail(), "1234");
+        VerificationMail mail = new VerificationMail(user.getEmail(), verificationCode);
         MailServiceProvider.getInstance().sendMail(mail);
 
         return "frontend/auth/login.jsp";

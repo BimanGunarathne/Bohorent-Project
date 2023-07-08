963e55ac6af4c52055cec2d9ed834138f02ffaa3
Biman Gunarathne <44740959+BimanGunarathne@users.noreply.github.com>
2023-07-08 17:42:23 +0530

    Updated

--- src/main/java/com/bohorent/shop/controllers/AddItemController.java
+++ src/main/java/com/bohorent/shop/controllers/AddItemController.java
@@ -10,12 +10,8 @@ import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import javax.persistence.NoResultException;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
 

e2816f28c423ab6e778c2a77bd5634ebfb8297cf
Biman Gunarathne <44740959+BimanGunarathne@users.noreply.github.com>
2023-08-09 09:10:14 +0530

    New added Entity BusinessSetting.java, update hibernate.cfg.xml

--- src/main/java/com/bohorent/shop/Test.java
+++ src/main/java/com/bohorent/shop/Test.java
@@ -1,9 +1,11 @@
 package com.bohorent.shop;
 
 import com.bohorent.shop.service.MailService;
+import com.bohorent.shop.util.HibernateUtil;
 
 public class Test {
     public static void main(String[] args) {
-        MailService.sendMail("bimangunarathne@gmail.com", null);
+        HibernateUtil.getSessionFactory().openSession();
+//        MailService.sendMail("bimangunarathne@gmail.com", null);
     }
 }
